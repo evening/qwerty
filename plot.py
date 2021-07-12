1228be9033995507078176c9786f6f062dc38949
unknown <39542044+evening@users.noreply.github.com>
2021-07-11 20:15:21 -0500

    support multiple words

--- plot.py
+++ plot.py
@@ -12,23 +12,26 @@ key_ys = list(map(lambda x: x[1], list(keyboard_map.values())))
 plt.scatter(key_xs, key_ys, s=1, c="black")
 
 # generate letters
-response = list(get_strokes(sys.argv[1]))
-xs = list(map(lambda x: x[1][0], response))
-ys = list(map(lambda x: x[1][1], response))
-
-plt.annotate(sys.argv[1], (200, 0), weight="bold")
-
 marked = {}
-for i, (c, (x, y)) in enumerate(response):
-    plt.annotate(
-        c,
-        (x, y),
-        textcoords="offset points",
-        xytext=(5, 5) if ((x, y) not in marked or marked[(x, y)] == c) else (-5, 5),
-        ha="center",
-        weight="bold" if i == 0 else "normal",
-    )
-    marked[(x, y)] = marked.get((x, y), c)
 
-plt.plot(xs, ys, ".r-")
+for i, word in enumerate(sys.argv[1:]):
+    response = list(get_strokes(word))
+    xs = list(map(lambda x: x[1][0], response))
+    ys = list(map(lambda x: x[1][1], response))
+
+    plt.annotate(word, (200, 20 * i), weight="bold")
+
+    for i, (c, (x, y)) in enumerate(response):
+        plt.annotate(
+            c,
+            (x, y),
+            textcoords="offset points",
+            xytext=(5, 5) if ((x, y) not in marked or marked[(x, y)] == c) else (-5, 5),
+            ha="center",
+            weight="bold" if i == 0 else "normal",
+        )
+        marked[(x, y)] = marked.get((x, y), c)
+
+    plt.plot(xs, ys, [".r-", ".g-", ".b-"][i % 3])
+
 plt.show()
